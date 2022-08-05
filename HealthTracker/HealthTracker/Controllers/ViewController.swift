8925e28f1584fbdb5149db456a777a127217d9b7
Cem Safa <cemsafa@gmail.com>
2022-08-05 14:44:00 -0400

    Initiated iOS project

---
+++ HealthTracker/HealthTracker/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthTracker
+//
+//  Created by Cem Safa on 2022-08-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
