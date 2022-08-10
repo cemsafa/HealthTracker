1de0b40bef2fd9b61d48b2ecf0d1831172af436f
Cem Safa <cemsafa@gmail.com>
2022-08-10 16:49:05 -0400

    iOS: Fixes on project

--- HealthTracker/HealthTracker/Controllers/LaunchScreenVC.swift
+++ HealthTracker/HealthTracker/Controllers/LaunchScreenVC.swift
@@ -32,17 +32,17 @@ class LaunchScreenVC: UIViewController {
     @objc private func showAppViewController() {
         
         if UserData.getToken() != nil {
-//            let storyboard = UIStoryboard.init(name: "Main", bundle: Bundle.main)
-//            let tabBarViewController = storyboard.instantiateViewController(withIdentifier: "TabBarViewController") as! TabBarViewController
-//            tabBarViewController.modalTransitionStyle = .crossDissolve
-//            tabBarViewController.modalPresentationStyle = .fullScreen
-//            self.present(tabBarViewController, animated: true, completion: nil)
+            let storyboard = UIStoryboard.init(name: "Main", bundle: Bundle.main)
+            let homeVC = storyboard.instantiateViewController(withIdentifier: "HomeVC") as! HomeVC
+            homeVC.modalTransitionStyle = .crossDissolve
+            homeVC.modalPresentationStyle = .fullScreen
+            self.present(homeVC, animated: true, completion: nil)
         } else {
-//            let storyboard = UIStoryboard.init(name: "Main", bundle: Bundle.main)
-//            let onBoardingVC = storyboard.instantiateViewController(withIdentifier: "OnBoardingVC") as! OnBoardingVC
-//            onBoardingVC.modalTransitionStyle = .crossDissolve
-//            onBoardingVC.modalPresentationStyle = .fullScreen
-//            self.present(onBoardingVC, animated: true, completion: nil)
+            let storyboard = UIStoryboard.init(name: "Main", bundle: Bundle.main)
+            let signupVC = storyboard.instantiateViewController(withIdentifier: "SignupVC") as! SignupVC
+            signupVC.modalTransitionStyle = .crossDissolve
+            signupVC.modalPresentationStyle = .fullScreen
+            self.present(signupVC, animated: true, completion: nil)
         }
     }
 }
