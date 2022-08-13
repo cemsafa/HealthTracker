c2b0f4f091fc9f8e212c52b60da4453a0117968f
Kristina Ginga <c0807287@mylambton.ca>
2022-08-13 16:23:19 -0400

    familyListUI done

---
+++ HealthTracker/HealthTracker/Controllers/AddFamilyVC.swift
@@ -0,0 +1,29 @@
+//
+//  AddFamilyVC.swift
+//  HealthTracker
+//
+//  Created by Ma. Kristina Ginga on 2022-08-13.
+//
+
+import UIKit
+
+class AddFamilyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
